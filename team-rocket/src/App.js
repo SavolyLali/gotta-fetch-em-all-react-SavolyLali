ffa47f9c3e97af03971b86ba8173ca5d857fe9e4
Daniel-Toth <danthe34@gmail.com>
2023-06-12 14:47:21 +0200

    solve selectedLocation fetch

--- team-rocket/src/App.js
+++ team-rocket/src/App.js
@@ -4,25 +4,30 @@ import Locations from './components/Locations';
 import FightLocation from './components/FightLocation';
 
 function App() {
-/*   const [url, setUrl] = useState('https://pokeapi.co/api/v2/location?offset=0&limit=20');
- */  const [data, setData] = useState(null);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/location?offset=0&limit=20');
+  const [data, setData] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [isClicked, setIsClicked] = useState(false);
 
   const handleCountryClick = (location) => {
     setSelectedLocation(location);
     setIsClicked(true);
+    setUrl(location.url)
   };
 
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/location?offset=0&limit=20')
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         console.log(data);
         setData(data);
+        if(data.areas){
+        setSelectedLocation(data.areas[0])
+        }
       });
   }, []);
+
 
 
   return (
--- team-rocket/src/components/FightLocation.js
+++ team-rocket/src/components/FightLocation.js
@@ -3,7 +3,7 @@ import React from 'react'
 function FightLocation({location}) {
   return (
     <div>
-        <h2>{location.areas[0].name}</h2>
+        <h2>{location.name}</h2>
     </div>
   )
 }
