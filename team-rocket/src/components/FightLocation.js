908584b35e2050b1fcd1e70864e823ff85bc07d1
Daniel-Toth <danthe34@gmail.com>
2023-06-14 13:43:59 +0200

    fixed merge

--- team-rocket/src/components/FightLocation.js
+++ team-rocket/src/components/FightLocation.js
@@ -2,11 +2,7 @@ import React from 'react'
 
 function FightLocation({location, onClick}) {
   return (
-<<<<<<< HEAD
-    <div id="fightLocation">
-=======
     <div className='fight-location'>
->>>>>>> 61f5822132eb6e15d74159572ee25c44a53ce581
         <h2>{location.name}</h2>
         <button onClick={onClick}>Back</button>
     </div>
